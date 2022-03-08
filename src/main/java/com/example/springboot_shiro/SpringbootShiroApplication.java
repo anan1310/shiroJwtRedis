403892b1414670e9fbb55f3fb2f4f77551da789b
anan1310 <anzj1997@163.com>
2022-03-08 23:05:01 +0800

    添加操作redis的工具类，静态注入JedisPool连接池进行连接

--- src/main/java/com/example/springboot_shiro/SpringbootShiroApplication.java
+++ src/main/java/com/example/springboot_shiro/SpringbootShiroApplication.java
@@ -3,11 +3,13 @@ package com.example.springboot_shiro;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @MapperScan("com.example.springboot_shiro.mapper")
+//@EnableConfigurationProperties
 public class SpringbootShiroApplication {
 
     public static void main(String[] args) {
